aae32a51af279966c6715f0a1091f3cb2aaf92ba
Germán David <manchov12@gmail.com>
2024-05-14 16:43:34 -0500

    Creando funcion eliminar

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import { Dispatch, useMemo } from "react"
 import { Activity } from "../types"
 import { categories } from "../data/categories"
-import { PencilSquareIcon } from '@heroicons/react/24/outline'
+import { PencilSquareIcon, XCircleIcon } from '@heroicons/react/24/outline'
 import { ActivityActions } from "../reducers/activity-reducer"
 
 type ActivityListProps = {
@@ -14,13 +14,16 @@ export default function ActivityList({activities, dispatch} : ActivityListProps 
     const categoryName = useMemo(() => 
     (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : '')
     , [activities])
+
+    const isEmptyActivities = useMemo(() => activities.length === 0, [activities])
     
     return (
     <>
         <h2 className="text-4xl font-bold text-slate-600 text-center">
             Comida y Actividades
         </h2>
-        {activities.map(activity => (
+        {isEmptyActivities ? <p className="my-5 text-center" >No hay actividades aún ...</p> : 
+        activities.map(activity => (
             <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between"  >
                 <div className="space-y-2 relative" >
                     <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>{ categoryName(+activity.category)}</p>
@@ -38,10 +41,17 @@ export default function ActivityList({activities, dispatch} : ActivityListProps 
                             className='h-8 w-8 text-gray-800'
                         />
                     </button>
+
+                    <button
+                        onClick={() => dispatch({type: "delete-activity", payload: {id: activity.id}})}
+                    >
+                        <XCircleIcon 
+                            className='h-8 w-8 text-red-500'
+                        />
+                    </button>
                 </div>
             </div>
         ))}
-
     </>
   )
 }
