40765952cc3521d69ca5ce0bbedb83513b03d8c2
Germán David <manchov12@gmail.com>
2024-05-14 16:25:43 -0500

    Creando funcionalidad de editar

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,16 +1,18 @@
 import { Activity } from "../types"
 
-export type ActivityActions = {
-    type: 'save-activity',
-    payload: { newActiviy : Activity }
-}
+export type ActivityActions = 
+    { type: 'save-activity', payload: { newActiviy : Activity }} |
+    { type: 'set-activeId', payload: { id : Activity['id'] }}
 
-type ActivityState = {
-    activities : Activity[]
+
+export type ActivityState = {
+    activities : Activity[],
+    activeId: Activity['id']
 }
 
 export const initialState : ActivityState = {
-    activities : []
+    activities : [],
+    activeId: ''
 }
 
 export const activityReducer = (
@@ -20,9 +22,25 @@ export const activityReducer = (
 
     if (action.type === 'save-activity') {
         // Este código maneja la lógica para manejar el state
+        
+        let updateActivities : Activity[] = []
+        if (state.activeId) {
+            updateActivities = state.activities.map(activity => activity.id === state.activeId ? action.payload.newActiviy : activity)
+        }else{
+            updateActivities = [...state.activities, action.payload.newActiviy]
+        }
+
         return{
             ...state,
-            activities: [...state.activities, action.payload.newActiviy]
+            activities: updateActivities,
+            activeId: ''
+        }
+    }
+
+    if (action.type === 'set-activeId' ){
+        return{
+            ...state,
+            activeId: action.payload.id
         }
     }
 
